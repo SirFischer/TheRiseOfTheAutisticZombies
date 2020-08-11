c85546bc888b47c71109c8a16a204b17084d5eb4
Marek <marek5886@hotmail.com>
2020-08-12 00:34:26 +0200

    cleanup

--- obj/StateManager/State.d
+++
@@ -1,9 +0,0 @@
-obj/StateManager/State.o: srcs/StateManager/State.cpp \
- includes/StateManager/State.hpp includes/System/Window.hpp \
- mfGUI/includes/mfGUI.hpp mfGUI/includes/widgets/Widget.hpp \
- mfGUI/includes/utils/Events.hpp \
- mfGUI/includes/widgetComponents/Background.hpp \
- mfGUI/includes/widgetComponents/EventManager.hpp \
- mfGUI/includes/widgetComponents/Text.hpp \
- mfGUI/includes/widgets/Image.hpp mfGUI/includes/widgets/Button.hpp \
- mfGUI/includes/widgets/Text.hpp includes/StateManager/StateAction.hpp
